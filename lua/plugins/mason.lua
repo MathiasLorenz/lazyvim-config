ea71c960691eb959942fdfeda9c1380db72aa7fa
Mathias Lorenz <lorenz.mathias@gmail.com>
2025-09-17 21:01:01 +0200

    Update lazyvim, rename mason-org

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,6 @@
 return {
   -- disable installing binaries from mason - use system binaries instead
-  { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "williamboman/mason.nvim", enabled = false },
-  { "jay-babu/mason-nvim-dap.nvim", enabled = false }
+  { "mason-org/mason-lspconfig.nvim", enabled = false },
+  { "mason-org/mason.nvim", enabled = false },
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
 }
