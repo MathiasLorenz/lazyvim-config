8b377184b80c897baddb4fb84548bacd6ad91ce5
Mathias Lorenz <lorenz.mathias@gmail.com>
2025-10-24 08:57:57 +0200

    Play with colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -13,3 +13,23 @@ return {
     },
   },
 }
+--
+-- Trippy but also cool
+-- return {
+--   {
+--     "maxmx03/fluoromachine.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--       local fm = require("fluoromachine")
+--
+--       fm.setup({
+--         glow = true,
+--         theme = "retrowave",
+--         transparent = true,
+--       })
+--
+--       vim.cmd.colorscheme("fluoromachine")
+--     end,
+--   },
+-- }
